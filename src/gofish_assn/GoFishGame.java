ff82cb582e2bfdae51d64bad1fe71be15b2a0dc3
Desiree Tang <desireeotang@utexas.edu>
2018-02-06 13:16:46 -0600

    Changed GoFishGame.java

--- src/gofish_assn/GoFishGame.java
+++ src/gofish_assn/GoFishGame.java
@@ -10,37 +10,37 @@ public class GoFishGame {
 
 	public GoFishGame() {
 
-		Deck d = new Deck();
-		d.shuffle();
+        Deck d = new Deck();
+        d.shuffle();
 
-		Player p1 = new Player("Desiree");
-		System.out.println("Player One: " + p1.getName());
+        Player p1 = new Player("Desiree");
+        System.out.println("Player One: " + p1.getName());
 
-		Player p2 = new Player("Isabelle");
-		System.out.println("Player Two: " + p2.getName());
+        Player p2 = new Player("Isabelle");
+        System.out.println("Player Two: " + p2.getName());
 
-		for(int i =0; i < 7; i++){
-			Card c1 = d.dealCard();
-			p1.addCardToHand(c1);
-			Card c2 = d.dealCard();
-			p2.addCardToHand(c2);
-		}
+        for (int i = 0; i < 7; i++) {
+            Card c1 = d.dealCard();
+            p1.addCardToHand(c1);
+            Card c2 = d.dealCard();
+            p2.addCardToHand(c2);
+        }
 
-		System.out.println(p1.getName() + "'s Hand: ");
-		System.out.println(p1.handToString());
-		System.out.println(p2.getName() + "'s Hand: ");
-		System.out.println(p2.handToString());
+        System.out.println(p1.getName() + "'s Hand: ");
+        System.out.println(p1.handToString());
+        System.out.println(p2.getName() + "'s Hand: ");
+        System.out.println(p2.handToString());
 
-		//Actual game starts here
+        //Actual game starts here
 
-        while(p1.checkHandForBook()){
+        while (p1.checkHandForBook()) {
             System.out.println(p1.getName() + " books initial pairs");
             System.out.println(p1.getName() + "'s Hand without Pairs: ");
             System.out.println(p1.handToString());
 
         }
 
-        while(p2.checkHandForBook()){
+        while (p2.checkHandForBook()) {
             System.out.println(p2.getName() + " books initial pairs");
             System.out.println(p2.getName() + "'s Hand without Pairs: ");
             System.out.println(p2.handToString());
@@ -48,115 +48,117 @@ public class GoFishGame {
 
 
         //for(int i =0; i< 10; i++){
-		while((p1.getBookSize() + p2.getBookSize() )!= 26){
-            while(p1.checkHandForBook()){}
-            while(p2.checkHandForBook()){}
-
-
-            if(p1.getHandSize() == 0 && !d.isEmpty()){ // hand is empty so draw card
-
-            if(p1.hand.isEmpty() && !d.isEmpty()){ // hand is empty so draw card
-                Card draw1 = d.dealCard();
-                p1.addCardToHand(draw1);
+        while ((p1.getBookSize() + p2.getBookSize()) != 26) {
+            while (p1.checkHandForBook()) {
+            }
+            while (p2.checkHandForBook()) {
             }
 
-            if(p1.getHandSize() != 0 && !d.isEmpty()) {
 
-                // Player 1's turn
-                //if deck is empty and hand is empty -> move on to next player
-                Card chosen1 = p1.chooseCardFromHand();
-                System.out.println(p1.getName() + " asks: Do you have a " + chosen1.getRank() + "?");
+            if (p1.getHandSize() == 0 && !d.isEmpty()) { // hand is empty so draw card
 
-                if (p2.rankInHand(chosen1)) {
-                    System.out.println(p2.getName() + " says: Yes. I do have a " + chosen1.getRank());
-                    Card removed1 = p2.removeCardFromHand(chosen1);
-                    p1.addCardToHand(removed1);
-                    p1.checkHandForBook();
-                    System.out.println(p1.getName() + " books the " + chosen1.getRank());
-                } else {
-                    System.out.println(p2.getName() + " says: Go Fish");
-                    if (d.isEmpty()) {
-                        System.out.println("No more cards in deck. Continue to next player's turn.");
+                if (p1.getHandSize() != 0 && !d.isEmpty()) { // hand is empty so draw card
+                    Card draw1 = d.dealCard();
+                    p1.addCardToHand(draw1);
+                }
+
+                if (p1.getHandSize() != 0 && !d.isEmpty()) {
+
+                    // Player 1's turn
+                    //if deck is empty and hand is empty -> move on to next player
+                    Card chosen1 = p1.chooseCardFromHand();
+                    System.out.println(p1.getName() + " asks: Do you have a " + chosen1.getRank() + "?");
+
+                    if (p2.rankInHand(chosen1)) {
+                        System.out.println(p2.getName() + " says: Yes. I do have a " + chosen1.getRank());
+                        Card removed1 = p2.removeCardFromHand(chosen1);
+                        p1.addCardToHand(removed1);
+                        p1.checkHandForBook();
+                        System.out.println(p1.getName() + " books the " + chosen1.getRank());
                     } else {
-                        Card nextCard = d.dealCard();
-                        System.out.println(p1.getName() + " draws " + nextCard.toString());
-                        p1.addCardToHand(nextCard);
+                        System.out.println(p2.getName() + " says: Go Fish");
+                        if (d.isEmpty()) {
+                            System.out.println("No more cards in deck. Continue to next player's turn.");
+                        } else {
+                            Card nextCard = d.dealCard();
+                            System.out.println(p1.getName() + " draws " + nextCard.toString());
+                            p1.addCardToHand(nextCard);
+                        }
+
                     }
 
                 }
 
-            }
+                if (p2.getHandSize() == 0 && !d.isEmpty()) {
+                    Card draw2 = d.dealCard();
+                    p2.addCardToHand(draw2);
+                }
 
-            if(p2.getHandSize() == 0 && !d.isEmpty()){
-                Card draw2 = d.dealCard();
-                p2.addCardToHand(draw2);
-            }
+                if (p2.getHandSize() != 0 && !d.isEmpty()) {
+                    // Player 2's Card
+                    Card chosen2 = p2.chooseCardFromHand();
+                    System.out.println(p2.getName() + " asks: Do you have a " + chosen2.getRank() + "?");
 
-            if(p2.getHandSize() != 0 && !d.isEmpty()) {
-                // Player 2's Card
-                Card chosen2 = p2.chooseCardFromHand();
-                System.out.println(p2.getName() + " asks: Do you have a " + chosen2.getRank() + "?");
-
-                if (p1.rankInHand(chosen2)) {
-                    System.out.println(p1.getName() + " says: Yes. I do have a " + chosen2.getRank());
-                    Card removed2 = p1.removeCardFromHand(chosen2);
-                    p2.addCardToHand(removed2);
-                    p2.checkHandForBook();
-                    System.out.println(p2.getName() + " books the " + chosen2.getRank());
-                } else {
-                    System.out.println(p1.getName() + " says: Go Fish");
-                    if (d.isEmpty()) {
-                        System.out.println("No more cards in deck. Continue to next player's turn.");
+                    if (p1.rankInHand(chosen2)) {
+                        System.out.println(p1.getName() + " says: Yes. I do have a " + chosen2.getRank());
+                        Card removed2 = p1.removeCardFromHand(chosen2);
+                        p2.addCardToHand(removed2);
+                        p2.checkHandForBook();
+                        System.out.println(p2.getName() + " books the " + chosen2.getRank());
                     } else {
-                        Card nextCard = d.dealCard();
-                        System.out.println(p1.getName() + " draws " + nextCard.toString());
-                        p2.addCardToHand(nextCard);
+                        System.out.println(p1.getName() + " says: Go Fish");
+                        if (d.isEmpty()) {
+                            System.out.println("No more cards in deck. Continue to next player's turn.");
+                        } else {
+                            Card nextCard = d.dealCard();
+                            System.out.println(p1.getName() + " draws " + nextCard.toString());
+                            p2.addCardToHand(nextCard);
+                        }
+                    }
+                }
+
+                if (d.isEmpty()) {
+                    // one player has all cards and the other player has none and deck is empty
+                    // if both player has cards and deck is empty
+                    while (p1.checkHandForBook()) {
+                    }
+                    while (p2.checkHandForBook()) {
+                    }
+                    if (p1.getBookSize() + p2.getBookSize() == 26) {
+                        break;
                     }
                 }
             }
 
-            if(d.isEmpty()){
-                // one player has all cards and the other player has none and deck is empty
-                // if both player has cards and deck is empty
-                while(p1.checkHandForBook()){}
-                while(p2.checkHandForBook()){}
-                if(p1.getBookSize() + p2.getBookSize() == 26){
-                    break;
-                }
+            // end of game: must have a total of 26 pairs in both books
+            if (p1.getBookSize() > p2.getBookSize()) {
+                System.out.println(p1.getName() + " wins with " + p1.getBookSize() + " booked pairs.");
+                System.out.println("These are " + p1.getName() + "'s pairs");
+                System.out.println(p1.bookToString());
+                System.out.println(p2.getName() + "has " + p2.getBookSize() + " booked pairs.");
+                System.out.println("These are " + p2.getName() + "'s pairs");
+                System.out.println(p2.bookToString());
+                System.out.println("Thanks for playing Go Fish!");
+            } else if (p1.getBookSize() < p2.getBookSize()) {
+                System.out.println(p2.getName() + " wins with " + p2.getBookSize() + " booked pairs.");
+                System.out.println("These are " + p2.getName() + "'s pairs");
+                System.out.println(p2.bookToString());
+                System.out.println(p1.getName() + "has " + p1.getBookSize() + " booked pairs.");
+                System.out.println("These are " + p1.getName() + "'s pairs");
+                System.out.println(p1.bookToString());
+                System.out.println("Thanks for playing Go Fish!");
+            } else if (p1.getBookSize() == p2.getBookSize()) {
+                System.out.println("Tie game!");
+                System.out.println(p1.getName() + " has " + p1.getBookSize() + " booked pairs");
+                System.out.println("These are " + p1.getName() + "'s pairs");
+                System.out.println(p1.bookToString());
+
+                System.out.println(p2.getName() + "has " + p2.getBookSize() + " booked pairs.");
+                System.out.println("These are " + p2.getName() + "'s pairs");
+                System.out.println(p2.bookToString());
+
+                System.out.println("Thanks for playing Go Fish!");
             }
-		}
-
-        // end of game: must have a total of 26 pairs in both books
-        if(p1.getBookSize() > p2.getBookSize()){
-            System.out.println(p1.getName() + " wins with " + p1.getBookSize() +" booked pairs.");
-            System.out.println("These are " + p1.getName() + "'s pairs");
-            System.out.println(p1.bookToString());
-            System.out.println(p2.getName() + "has " + p2.getBookSize() + " booked pairs.");
-            System.out.println("These are " + p2.getName() + "'s pairs");
-            System.out.println(p2.bookToString());
-            System.out.println("Thanks for playing Go Fish!");
         }
-        else if(p1.getBookSize() < p2.getBookSize()){
-            System.out.println(p2.getName() + " wins with " + p2.getBookSize() +" booked pairs.");
-            System.out.println("These are " + p2.getName() + "'s pairs");
-            System.out.println(p2.bookToString());
-            System.out.println(p1.getName() + "has " + p1.getBookSize() + " booked pairs.");
-            System.out.println("These are " + p1.getName() + "'s pairs");
-            System.out.println(p1.bookToString());
-            System.out.println("Thanks for playing Go Fish!");
-        }
-        else if(p1.getBookSize() == p2.getBookSize()){
-            System.out.println("Tie game!");
-            System.out.println(p1.getName() + " has " + p1.getBookSize() + " booked pairs");
-            System.out.println("These are " + p1.getName() + "'s pairs");
-            System.out.println(p1.bookToString());
-
-            System.out.println(p2.getName() + "has " + p2.getBookSize() + " booked pairs.");
-            System.out.println("These are " + p2.getName() + "'s pairs");
-            System.out.println(p2.bookToString());
-
-            System.out.println("Thanks for playing Go Fish!");
-        }
-	}
-
+    }
 }
